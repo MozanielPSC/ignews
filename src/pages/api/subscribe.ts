53a9142b35f7b9969f67e395f3ae5199b555bafb
Mozaniel Pereira e Silva Corrêa <mozanielpcorrea@gmail.com>
2022-01-26 10:09:57 -0300

    Fuckeslint

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from 'next-auth/client';
-import { fauna } from "../../services/fauna";
-import { query as q } from "faunadb";
+import { getSession } from 'next-auth/client'
+
+import { query as q } from 'faunadb'
+
 import { stripe } from "../../services/stripe";
+import { fauna } from "../../services/fauna";
+
 type User = {
     ref: {
         id: string;
-    }
+    },
     data: {
-        stripe_customer_id: string;
+        stripe_customer_id
     }
 }
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const session = await getSession({ req });
+
         const user = await fauna.query<User>(
             q.Get(
                 q.Match(
@@ -24,12 +30,12 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         )
 
         let customerId = user.data.stripe_customer_id;
+
         if (!customerId) {
             const stripeCustomer = await stripe.customers.create({
-                email: session.user.email,
-                //metadata
-
+                email: session.user.email
             })
+
             await fauna.query(
                 q.Update(
                     q.Ref(q.Collection('users'), user.ref.id),
@@ -40,24 +46,26 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
                     }
                 )
             )
-            customerId = stripeCustomer.id;
+
+            customerId = stripeCustomer.id
         }
 
-
-
-        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+        const stripeCheckouSession = await stripe.checkout.sessions.create({
             customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
-                { price: 'price_1KBfCpCp5IDLElFbdM69blhS', quantity: 1 }
+                {
+                    price: 'price_1IXnITCzujPzpToy7vaDltBy', quantity: 1
+                }
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
             success_url: process.env.STRIPE_SUCCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
-        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+
+        return res.status(200).json({ sessionId: stripeCheckouSession.id })
     } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method not allowed');
