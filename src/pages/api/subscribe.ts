d0dd29db8654725583efab31e03ddb16dffb7157
Mozaniel Pereira e Silva Corrêa <mozanielpcorrea@gmail.com>
2022-01-26 09:55:33 -0300

    env

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {getSession} from 'next-auth/client';
+import { getSession } from 'next-auth/client';
 import { fauna } from "../../services/fauna";
 import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
-type User ={
-    ref:{
-        id:string;
+type User = {
+    ref: {
+        id: string;
     }
-    data:{
+    data: {
         stripe_customer_id: string;
     }
 }
-export default async function(req:NextApiRequest,res:NextApiResponse){
-    if(req.method === 'POST'){
-        const session = await getSession({req});
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const session = await getSession({ req });
         const user = await fauna.query<User>(
             q.Get(
                 q.Match(
@@ -24,42 +24,42 @@ export default async function(req:NextApiRequest,res:NextApiResponse){
         )
 
         let customerId = user.data.stripe_customer_id;
-        if(!customerId){
+        if (!customerId) {
             const stripeCustomer = await stripe.customers.create({
                 email: session.user.email,
                 //metadata
-    
+
             })
             await fauna.query(
                 q.Update(
-                    q.Ref(q.Collection('users'),user.ref.id),
+                    q.Ref(q.Collection('users'), user.ref.id),
                     {
-                        data:{
-                            stripe_customer_id:stripeCustomer.id
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
                         }
                     }
                 )
             )
             customerId = stripeCustomer.id;
         }
-        
 
-       
+
+
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
-                {price:'price_1KBfCpCp5IDLElFbdM69blhS',quantity: 1}
+                { price: 'price_1KBfCpCp5IDLElFbdM69blhS', quantity: 1 }
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
-            success_url: 'http://localhost:3000/posts',
-            cancel_url: 'http://localhost:3000/'
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
         })
-        return res.status(200).json({sessionId: stripeCheckoutSession.id})
-    }else{
-        res.setHeader('Allow','POST');
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+    } else {
+        res.setHeader('Allow', 'POST');
         res.status(405).end('Method not allowed');
     }
 }
