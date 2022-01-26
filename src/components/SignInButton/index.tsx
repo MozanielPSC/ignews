faa62defd5c1bbccf47530d1c6903e75704318e6
Mozaniel Pereira e Silva Corrêa <mozanielpcorrea@gmail.com>
2022-01-26 08:04:35 -0300

    Prod

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,10 @@
 import { FaGithub } from "react-icons/fa"
 import { FiX } from "react-icons/fi"
 import styles from "./styles.module.scss"
-import { signIn, signOut,useSession } from "next-auth/client"
+import { signIn, signOut, useSession } from "next-auth/client"
 export function SignInButton() {
-    const [ session] = useSession()
-    return session ? (<button type="button" onClick={()=> signOut()} className={styles.signInButton}>
+    const [session] = useSession()
+    return session ? (<button type="button" onClick={() => signOut()} className={styles.signInButton}>
         <FaGithub color="#04d361"></FaGithub>
         {session.user.name}
         <FiX color="#737380" className={styles.closeIcon}></FiX>
